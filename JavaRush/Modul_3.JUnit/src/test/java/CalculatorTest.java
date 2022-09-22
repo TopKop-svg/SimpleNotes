6f589eaee5c42248cf65a665f1bbf47409090cf3
Top Cop <loookintome@gmail.com>
2022-09-22 19:36:01 +0900

    just check, no info. File is empty

---
+++ JavaRush/Modul_3.JUnit/src/test/java/CalculatorTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void add() {
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void sub() {
+        Calculator calc = new Calculator();
+        int result = calc.sub(10, 10);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void mul() {
+        Calculator calc = new Calculator();
+        int result = calc.mul(-5, -3);
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void div() {
+        Calculator calc = new Calculator();
+        int result = calc.div(2, 3);
+        assertEquals(0, result);
+    }
+}
